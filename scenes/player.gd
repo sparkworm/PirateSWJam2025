d87dff4d6506d55307f0691d7b7b43b79864d150
Zentroz <ay597555@gmail.com>
2025-01-22 05:30:53 +0530

    implemented monkey ai and punch
    made the player move left and right and punch if an enemy in its way.

---
+++ scenes/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@export var move_speed : float  = 300
+@export var jump_force = 500
+
+var direction_x: int = 1
+var direction: float 
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	movement(delta)
+
+func movement(delta: float) -> void:
+	# Applying gravity
+	if !is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Moving in a direction if on floor
+	if is_on_floor():
+		direction = direction_x * move_speed
+	else:
+		direction = 0
+	
+	velocity.x += direction
+	move_and_slide()
+	velocity.x -= direction # Subtracting direction so player don't build up speed
+
+
+func _on_platform_right_exited(body: Node2D) -> void: # Checking for platform edge on right
+	if !is_on_floor(): return
+	direction_x = -1
+
+
+func _on_platform_left_exited(body: Node2D) -> void: # Checking for platform edge on left
+	if !is_on_floor(): return
+	direction_x = 1
+
+
+func _on_punch_left_entered(body: Node2D) -> void: # Checking for enemy on left side
+	body.queue_free()
+
+
+func _on_punch_right_entered(body: Node2D) -> void: # Checking for enemy on right side
+	body.queue_free()
