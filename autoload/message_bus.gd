5e5db7f9b477f13e15fb632b1a6974a8570ca366
Sparkworm <imnota.squirp@gmail.com>
2025-01-24 10:17:13 -0600

    Added messages for when a lure is deployed or retracted

--- autoload/message_bus.gd
+++ autoload/message_bus.gd
@@ -6,3 +6,8 @@ signal change_scene(new_scene: Globals.SceneNames)
 
 ## Signal emitted when the game should be quit
 signal game_quit
+
+## signal emitted when a lure is added to the level
+signal lure_deployed(lure: Node2D)
+## signal emitted when a lure is retracted from the level
+signal lure_retracted(lure: Node2D)
